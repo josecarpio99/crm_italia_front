1d55f4278b774eca2675aff7330eb999de6a1b4a
jose chirino <josechirinocarpio@gmail.com>
2023-05-20 21:06:13 -0400

    added files

--- src/main.js
+++ src/main.js
@@ -1,5 +1,35 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+window.AppConfig = {
+  name: 'CRM_ITALIA',
+  // logo: 'logo',
+  url: import.meta.env.VITE_APP_BASE_URL,
+  csrf: '{{ csrf_token() }}',
+  defaultLocale: 'en',
+  defaultTimezone: 'America/Mexico_City',  
+}
 
-createApp(App).mount('#app')
+
+import {createApp} from 'vue'
+import { createPinia } from 'pinia'
+
+
+       
+
+import router from "@/router";
+import i18n from "@/plugins/i18n";
+import App from "@/App";
+
+import "./styles/main.css";
+
+
+const app = createApp(App)
+
+app.use(createPinia());
+
+app.use(router);
+app.use(i18n);
+
+app.mount('#app');
+
+
+
+
---
+++ src/plugins/axios.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+// axios.defaults.headers.common['X-Locale'] = localStorage.hasOwnProperty('locale') ? localStorage.locale : window.AppConfig.defaultLocale;
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+
+export default axios;
