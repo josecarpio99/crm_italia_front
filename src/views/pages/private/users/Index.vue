808eb7907432feb6cd4125c2d70c667fc90eced9
jose chirino <josechirinocarpio@gmail.com>
2023-05-28 08:46:45 -0400

    refactor uers/Index to script setup

--- src/views/pages/private/users/Index.vue
+++ src/views/pages/private/users/Index.vue
@@ -59,7 +59,7 @@
     </Page>
 </template>
 
-<script>
+<script setup>
 
 import {trans} from "@/helpers/i18n";
 import UserService from "@/services/UserService";
@@ -80,206 +80,177 @@ import TextInput from "@/views/components/input/TextInput";
 import Dropdown from "@/views/components/input/Dropdown";
 import {roleOptions} from "@/stub/roles";
 
-export default defineComponent({
-    components: {
-        Dropdown,
-        TextInput,
-        FiltersCol,
-        FiltersRow,
-        Filters,
-        Page,
-        Table2,
-        Avatar,
-        TableCell
-    },
-    setup() {
-        const service = new UserService();
-        const alertStore = useAlertStore();
-   
-        const mainQuery = reactive({
-            page: 1,
-            search: '',
-            sort: '',
-            filters: {
-                search: {
-                    value: '',
-                    comparison: '='
-                },              
-                role: {
-                    value: '',
-                    comparison: '='
-                }
-            }
-        });
+const service = new UserService();
+const alertStore = useAlertStore();
 
-        const page = reactive({
-            id: 'list_users',
-            title: trans('global.pages.users'),
-            breadcrumbs: [
-                {
-                    name: trans('global.pages.users'),
-                    to: toUrl('/users'),
-                    active: true,
-                }
-            ],
-            actions: [
-                {
-                    id: 'filters',
-                    name: trans('global.buttons.filters'),
-                    icon: "fa fa-filter",
-                    theme: 'outline',
-                },
-                {
-                    id: 'new',
-                    name: trans('global.buttons.add_new'),
-                    icon: "fa fa-plus",
-                    to: toUrl('/users/create')
-                }
-            ],
-            toggleFilters: false,
-        });
-
-        const table = reactive({
-            headers: {
-                // id: trans('users.labels.id_pound'),
-                name: trans('users.labels.name'),
-                email: trans('users.labels.email'),
-                role: trans('users.labels.role'),
-            },
-            sorting: {
-                name: true,
-                email: true
-            },
-            columns: [
-                {
-                    key: 'name',
-                    label: trans('users.labels.name'),
-                    editable: true,
-                    sorteable: true
-                },
-                {
-                    key: 'email',
-                    label: trans('users.labels.email'),
-                    sorteable: true
-                },
-                {
-                    key: 'role',
-                    label: trans('users.labels.role'),
-                    sortable: false,
-                },
-                
-            ],           
-            pagination: {
-                meta: null,
-                links: null,
-            },
-            actions: {
-                edit: {
-                    id: 'edit',
-                    name: trans('global.actions.edit'),
-                    icon: "fa fa-edit",
-                    showName: false,
-                    to: toUrl('/users/{id}/edit')
-                },
-                delete: {
-                    id: 'delete',
-                    name: trans('global.actions.delete'),
-                    icon: "fa fa-trash",
-                    showName: false,
-                    danger: true,
-                }
-            },
-            loading: false,
-            records: null,
-            editableFields: {
-                name: {
-                    type: 'input'
-                }
-            }
-        })  
-
-        function onTableSort(params) {
-            mainQuery.sort = params;
+const mainQuery = reactive({
+    page: 1,
+    search: '',
+    sort: '',
+    filters: {
+        search: {
+            value: '',
+            comparison: '='
+        },              
+        role: {
+            value: '',
+            comparison: '='
         }
-
-        function onTablePageChange(page) {
-            mainQuery.page = page;
-        }
-
-        function onTableAction(params) {
-            switch (params.action.id) {
-                case 'delete':
-                    alertHelpers.confirmDanger(function () {
-                        service.delete(params.item.id).then(function (response) {
-                            fetchPage(mainQuery);
-                        });
-                    })
-                    break;
-            }
-        }
-
-        function onPageAction(params) {           
-            switch (params.action.id) {
-                case 'filters':
-                    page.toggleFilters = !page.toggleFilters;
-                    break;
-            }
-        }
-
-        function onFiltersClear() {
-            let clonedFilters = mainQuery.filters;
-            for(let key in clonedFilters) {
-                clonedFilters[key].value = '';
-            }
-            mainQuery.filters = clonedFilters;
-        }
-
-        function fetchPage(params) {
-            table.records = [];
-            table.loading = true;
-            let query = prepareQuery(params);
-            service
-                .index(query)
-                .then((response) => {
-                    table.records = response.data.data;
-                    table.pagination.meta = response.data.meta;
-                    table.pagination.links = response.data.links;
-                    table.loading = false;
-                })
-                .catch((error) => {
-                    alertStore.error(getResponseError(error));
-                    table.loading = false;
-                });
-        }
-
-        function handleCellChange(payload) {
-            const record = table.records.find((item) => item.id == payload.record.id);          
-            record[payload.key] = typeof payload.value == 'object' ? payload.value.id : payload.value.toString();                   
-            service.handleUpdate(page.id, record.id, record);
-        }
-
-        watch(mainQuery, (newTableState) => {
-            fetchPage(mainQuery);
-        });
-
-        onMounted(() => {
-            fetchPage(mainQuery);
-        });
-
-        return {
-            roleOptions,
-            trans,
-            page,
-            table,
-            onTablePageChange,
-            onTableAction,
-            onTableSort,
-            onPageAction,
-            onFiltersClear,
-            mainQuery,
-            handleCellChange
-        }
-
-    },
+    }
 });
+
+const page = reactive({
+    id: 'list_users',
+    title: trans('global.pages.users'),
+    breadcrumbs: [
+        {
+            name: trans('global.pages.users'),
+            to: toUrl('/users'),
+            active: true,
+        }
+    ],
+    actions: [
+        {
+            id: 'filters',
+            name: trans('global.buttons.filters'),
+            icon: "fa fa-filter",
+            theme: 'outline',
+        },
+        {
+            id: 'new',
+            name: trans('global.buttons.add_new'),
+            icon: "fa fa-plus",
+            to: toUrl('/users/create')
+        }
+    ],
+    toggleFilters: false,
+});
+
+const table = reactive({
+    headers: {
+        // id: trans('users.labels.id_pound'),
+        name: trans('users.labels.name'),
+        email: trans('users.labels.email'),
+        role: trans('users.labels.role'),
+    },
+    sorting: {
+        name: true,
+        email: true
+    },
+    columns: [
+        {
+            key: 'name',
+            label: trans('users.labels.name'),
+            editable: true,
+            sorteable: true
+        },
+        {
+            key: 'email',
+            label: trans('users.labels.email'),
+            sorteable: true
+        },
+        {
+            key: 'role',
+            label: trans('users.labels.role'),
+            sortable: false,
+        },
+        
+    ],           
+    pagination: {
+        meta: null,
+        links: null,
+    },
+    actions: {
+        edit: {
+            id: 'edit',
+            name: trans('global.actions.edit'),
+            icon: "fa fa-edit",
+            showName: false,
+            to: toUrl('/users/{id}/edit')
+        },
+        delete: {
+            id: 'delete',
+            name: trans('global.actions.delete'),
+            icon: "fa fa-trash",
+            showName: false,
+            danger: true,
+        }
+    },
+    loading: false,
+    records: null,
+    editableFields: {
+        name: {
+            type: 'input'
+        }
+    }
+})  
+
+function onTableSort(params) {
+    mainQuery.sort = params;
+}
+
+function onTablePageChange(page) {
+    mainQuery.page = page;
+}
+
+function onTableAction(params) {
+    switch (params.action.id) {
+        case 'delete':
+            alertHelpers.confirmDanger(function () {
+                service.delete(params.item.id).then(function (response) {
+                    fetchPage(mainQuery);
+                });
+            })
+            break;
+    }
+}
+
+function onPageAction(params) {           
+    switch (params.action.id) {
+        case 'filters':
+            page.toggleFilters = !page.toggleFilters;
+            break;
+    }
+}
+
+function onFiltersClear() {
+    let clonedFilters = mainQuery.filters;
+    for(let key in clonedFilters) {
+        clonedFilters[key].value = '';
+    }
+    mainQuery.filters = clonedFilters;
+}
+
+function fetchPage(params) {
+    table.records = [];
+    table.loading = true;
+    let query = prepareQuery(params);
+    service
+        .index(query)
+        .then((response) => {
+            table.records = response.data.data;
+            table.pagination.meta = response.data.meta;
+            table.pagination.links = response.data.links;
+            table.loading = false;
+        })
+        .catch((error) => {
+            alertStore.error(getResponseError(error));
+            table.loading = false;
+        });
+}
+
+function handleCellChange(payload) {
+    const record = table.records.find((item) => item.id == payload.record.id);          
+    record[payload.key] = typeof payload.value == 'object' ? payload.value.id : payload.value.toString();                   
+    service.handleUpdate(page.id, record.id, record);
+}
+
+watch(mainQuery, (newTableState) => {
+    fetchPage(mainQuery);
+});
+
+onMounted(() => {
+    fetchPage(mainQuery);
+});
+
 </script>
