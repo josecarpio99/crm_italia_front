84a0e03ceb1d9931d76892dd44d84a3059b77aae
jose chirino <josechirinocarpio@gmail.com>
2023-06-01 09:21:33 -0400

    update createPersonModal

--- src/views/components/BaseModal.vue
+++ src/views/components/BaseModal.vue
@@ -8,7 +8,7 @@
         <Transition name="modal-inner">
           <div
             v-if="modalActive"
-            class="p-4 bg-white self-start mt-32 max-w-screen-md"
+            class="p-4 bg-white self-start mt-32 max-w-screen-lg"
           >
             <slot />
             <button
--- src/views/pages/private/customers/modals/CreatePersonModal.vue
+++ src/views/pages/private/customers/modals/CreatePersonModal.vue
@@ -1,11 +1,28 @@
 <template>
   <BaseModal @close-modal="$emit('close-modal')">
-    <h1>Hi...</h1>
+    <Form id="create-user" @submit.prevent="onSubmit" class="w-[640px]">
+      <TextInput class="mb-4" type="text" :required="true" name="name" v-model="form.name" :label="trans('global.labels.name')"/>             
+      <TextInput class="mb-4" type="email" :required="true" name="email" v-model="form.email" :label="trans('global.labels.email')"/>
+  </Form>
   </BaseModal>
 </template>
 
 <script setup>
-import BaseModal from '@/views/components/BaseModal'
+import {reactive} from "vue";
+import {trans} from "@/helpers/i18n";
+import BaseModal from '@/views/components/BaseModal';
+import Form from "@/views/components/Form";
+import TextInput from "@/views/components/input/TextInput";
 
 defineEmits(["close-modal"]);
+
+const form = reactive({
+  name: '',           
+  email: '',
+});
+
+function onSubmit() {
+  console.log('llego');
+}
+
 </script>
