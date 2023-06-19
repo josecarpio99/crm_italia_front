c16d374eef7632f835f742b7b816f28baa9f63ff
jose chirino <josechirinocarpio@gmail.com>
2023-06-19 10:04:09 -0400

    update dashboard | Added iframe

--- src/views/pages/private/dashboard/Main.vue
+++ src/views/pages/private/dashboard/Main.vue
@@ -1,10 +1,9 @@
 <template>
     <Page>
-        <div class="w-1/2 m-auto text-center pt-10">
-            <h1 class="text-6xl mb-4 font-bold text-gray-600">{{ trans('global.pages.welcome') }}</h1>
-        </div>
-        <footer class="py-5 text-center">
-        </footer>
+        <!-- <div class="w-1/2 m-auto text-center pt-6">
+            <h1 class="text-2xl mb-8 font-bold text-gray-600">{{ trans('global.pages.welcome') }}</h1>
+        </div> -->
+        <iframe class="w-full min-h-[90vh] mt-8" src="https://ranking.lineaitalia.net/ranking.php" title="Ranking vendedores"></iframe>
     </Page>
 </template>
 
