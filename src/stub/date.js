486a0254c9de19a8b5a0b2d966a6b0b0ca914924
jose chirino <josechirinocarpio@gmail.com>
2023-06-22 08:51:08 -0400

    added date stub

---
+++ src/stub/date.js
@@ -0,0 +1,12 @@
+export const dateStrFilter = [
+  'today',
+  'yesterday',
+  'this_week',
+  'last_week',
+  'this_month',
+  'last_month',
+  'this_quarter',
+  'last_quarter',
+  'this_year',
+  'last_year'
+];
