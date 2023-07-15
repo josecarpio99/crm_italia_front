2a404e774d9f154ad640fc5a2100d62307dd4f4a
jose chirino <josechirinocarpio@gmail.com>
2023-07-15 12:40:35 -0400

    update feed store

--- src/stores/feed.js
+++ src/stores/feed.js
@@ -14,11 +14,13 @@ export const useFeedStore = defineStore("feed", {
       const taskStore = useTaskStore();
       const noteStore = useNoteStore();
       const notes = noteStore.notes.map(note => {
+        note.type = 'note';
         note.sort_date = note.created_at;
         return note;
       });
 
       const tasks = taskStore.tasks.map(task => {
+        task.type = 'task';
         task.sort_date = task.due_date;
         return task;
       });
