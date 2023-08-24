5cc6c3c2904250c8988048a8168cdded577c2ed9
jose chirino <josechirinocarpio@gmail.com>
2023-08-24 10:06:56 -0400

    update dropdowns color design

--- src/views/components/input/Dropdown.vue
+++ src/views/components/input/Dropdown.vue
@@ -262,52 +262,62 @@ div[data-value="Referido"] .vs__selected {
 
 // #################################
 
-div[data-value="AAA - Corporativos e Industrias"] .vs__selected {
-    @apply bg-yellow-300;
-    @apply text-gray-600;
-}
-
-div[data-value="AA - Pymes"] .vs__selected,
+div[data-value="AAA - Corporativos e Industrias"] .vs__selected,
 div[data-value="de 0 a 3 meses"] .vs__selected
 {
     @apply bg-green-300;
     @apply text-gray-600;
 }
 
-div[data-value="A - Micro y Personas Físicas"] .vs__selected,
+div[data-value="AA - Pymes"] .vs__selected,
 div[data-value="de 3 a 6 meses"] .vs__selected
+{
+    @apply bg-yellow-300;
+    @apply text-gray-600;
+}
+
+div[data-value="A - Micro y Personas Físicas"] .vs__selected,
+div[data-value="6 meses o más"] .vs__selected
 {
     @apply bg-blue-300;
     @apply text-gray-600;
 }
 
-div[data-value="6 meses o más"] .vs__selected
+// div[data-value="6 meses o más"] .vs__selected
+// {
+//     @apply bg-red-400;
+//     @apply text-gray-600;
+// }
+
+.vs__dropdown-menu li:hover {
+    @apply opacity-80;
+}
+
+.deal_category .vs__dropdown-menu li:nth-child(1),
+.estimated_close_date_range .vs__dropdown-menu li:nth-child(1) 
 {
-    @apply bg-red-400;
+    @apply bg-green-300;
     @apply text-gray-600;
 }
 
-.deal_category .vs__dropdown-menu li:nth-child(1) {
-    @apply text-yellow-300;
-}
-
 .deal_category .vs__dropdown-menu li:nth-child(2),
-.estimated_close_date_range .vs__dropdown-menu li:nth-child(1) 
+.estimated_close_date_range .vs__dropdown-menu li:nth-child(2) 
 {
-    @apply text-green-300;
+    @apply bg-yellow-300;
+    @apply text-gray-600;
 }
 
 .deal_category .vs__dropdown-menu li:nth-child(3),
-.estimated_close_date_range .vs__dropdown-menu li:nth-child(2),
-.deal_source .vs__dropdown-menu li
-{
-    @apply text-blue-300;
-}
-
 .estimated_close_date_range .vs__dropdown-menu li:nth-child(3) 
 {
-    @apply text-red-400;
+    @apply bg-blue-300;
+    @apply text-gray-600;
 }
+
+// .estimated_close_date_range .vs__dropdown-menu li:nth-child(3) 
+// {
+//     @apply text-red-400;
+// }
 
 
 </style>
