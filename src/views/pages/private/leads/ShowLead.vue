c67607e0bc5935e0859697b66c06d6c3f42d9414
jose chirino <josechirinocarpio@gmail.com>
2023-09-29 12:45:34 -0400

    update lead details page

--- src/views/pages/private/leads/ShowLead.vue
+++ src/views/pages/private/leads/ShowLead.vue
@@ -12,7 +12,9 @@
       <div class="flex items-center ml-6" v-if="lead">
         <slot v-for="(action, j) in page.actions" :name="'page-actions-'+action.id">
             <Button v-if="action.hasOwnProperty('to') && action.to" :class="{'mr-3' : j < (page.actions.length-1)}" class="py-[.375rem]" :to="action.to" :title="action.name" :icon="action.hasOwnProperty('icon') ? action.icon : null" :theme="action.hasOwnProperty('theme') ? action.theme : null" :label="action.name"/>
-            <Button v-else @click="onPageAction({action: action})" :class="{'mr-3' : j < (page.actions.length-1)}" class="py-[.375rem]" :title="action.name" :icon="action.hasOwnProperty('icon') ? action.icon : null" :theme="action.hasOwnProperty('theme') ? action.theme : null" :label="action.name"/>
+            <Button v-else @click="onPageAction({action: action})" :class="{'mr-3' : j < (page.actions.length-1)}" class="py-[.375rem]" :title="action.name" :icon="action.hasOwnProperty('icon') ? action.icon : null" :theme="action.hasOwnProperty('theme') ? action.theme : null" :label="action.name"
+            v-tooltip.right="trans('leads.phrases.convert_lead')"
+            />
         </slot>
       </div>
     </template>
